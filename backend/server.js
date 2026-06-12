17ce9351ce5d779d4645137df4fd9fde80850080
vismaya-10 <vinuaishu07@gmail.com>
2026-06-12 11:57:05 +0530

    fix CORS for profuction

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({ 
-  origin: ['http://localhost:5173', 'https://spendlens.vercel.app'],
+  origin: ['http://localhost:5173', 'https://spendlens-khaki.vercel.app'],
   credentials: true 
 }));
 app.use(express.json());
