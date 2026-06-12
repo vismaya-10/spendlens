108426107d3935b2b122ad6ce3625793d4f7e7bc
vismaya-10 <vinuaishu07@gmail.com>
2026-06-12 12:08:49 +0530

    fix port and cors for render

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,8 @@ app.use('/api/anomalies', require('./routes/anomalies'));
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
+    app.listen(process.env.PORT || 5000, () => 
+  console.log(`Server running on port ${process.env.PORT || 5000}`)
+);
   })
   .catch(err => console.error('DB Error:', err));
